a29898371f9c18d2be3ca347464c01ceb0c8d685
sss <sss@karma>
2025-09-06 11:27:47 -0400

    Missing namespace qualifiers.

--- include/TrackToPFOConverterProcessor.h
+++ include/TrackToPFOConverterProcessor.h
@@ -31,14 +31,14 @@ class TrackToPFOConverterProcessor : public Processor {
 
   /** Called for every run.
    */
-  virtual void processRunHeader( LCRunHeader* run ) ;
+  virtual void processRunHeader( lcio::LCRunHeader* run ) ;
 
   /** Called for every event - the working horse.
    */
-  virtual void processEvent( LCEvent* evt ) ;
+  virtual void processEvent( lcio::LCEvent* evt ) ;
 
 
-  virtual void check( LCEvent* evt ) ;
+  virtual void check( lcio::LCEvent* evt ) ;
 
 
   /** Called after data processing for clean up.
